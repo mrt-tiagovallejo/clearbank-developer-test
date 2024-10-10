01438bb29313382b40a6676ca4d908e14653a418
Tiago Vallejo <tiago@tvallejo.net>
2024-10-11 00:30:24 +0100

    Added AccountDataStoreFactory unit tests

--- ClearBank.DeveloperTest.Tests/AccountDataStoreFactoryTests.cs
+++ ClearBank.DeveloperTest.Tests/AccountDataStoreFactoryTests.cs
@@ -1,14 +1,50 @@
-﻿using NUnit.Framework;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ClearBank.DeveloperTest.Data;
+using ClearBank.DeveloperTest.Data.Interfaces;
+using Moq;
+using NUnit.Framework;
 
 namespace ClearBank.DeveloperTest.Tests
 {
     public class AccountDataStoreFactoryTests
     {
-        // TODO
+        private Mock<IConfigurationManager> _mockConfigurationManager;
+        private AccountDataStoreFactory _accountDataStoreFactory;
+
+        [SetUp]
+        public void Setup()
+        {
+            _mockConfigurationManager = new Mock<IConfigurationManager>();
+            _accountDataStoreFactory = new AccountDataStoreFactory(_mockConfigurationManager.Object);
+        }
+
+        [Test]
+        public void GetAccountDataStore_ReturnsBackupAccountDataStore_WhenDataStoreTypeIsBackup()
+        {
+            // Arrange
+            _mockConfigurationManager
+                .Setup(cm => cm.GetAppSetting("DataStoreType"))
+                .Returns("Backup");
+
+            // Act
+            var result = _accountDataStoreFactory.GetAccountDataStore();
+
+            // Assert
+            Assert.That(result, Is.TypeOf<BackupAccountDataStore>());
+        }
+
+        [Test]
+        public void GetAccountDataStore_ReturnsAccountDataStore_WhenUnknownDataStoreType()
+        {
+            // Arrange
+            _mockConfigurationManager
+                .Setup(cm => cm.GetAppSetting("DataStoreType"))
+                .Returns(string.Empty);
+
+            // Act
+            var result = _accountDataStoreFactory.GetAccountDataStore();
+
+            // Assert
+            Assert.That(result, Is.TypeOf<AccountDataStore>());
+        }
     }
 }
